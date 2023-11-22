ad112cb1dfa2d5a9bda484d1b8b4fbe22b66494b
Lisandro Vicens <93133502+leesan2000@users.noreply.github.com>
2023-11-22 16:28:14 +0100

    panel nombre joueurs

--- Assets/Scripts/ButtonSFX.cs
+++ Assets/Scripts/ButtonSFX.cs
@@ -2,20 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ButtonSFX : MonoBehaviour
 {
 
     private AudioSource audio;
+    [SerializeField]
+    private GameObject panel;
 
-    private void Start()
+    [SerializeField]
+    public Button yourButton;
+
+    void Start()
     {
         audio = GetComponent<AudioSource>();
+
+        yourButton.onClick.AddListener(TaskOnClick);
+
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        panel.SetActive(true);
     }
 
     public void playSFX()
     {
         audio.Play();
+    }
+
+    public void modalPlayers()
+    {
+        panel.SetActive(true);
     }
 
 
