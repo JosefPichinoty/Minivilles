9a3041f602a0e3c15c5045c2edbe7d0bdb1b2941
Bramzzrr <bramzz.frtn@gmail.com>
2023-11-24 09:17:59 +0100

    Shop Ended

--- Assets/Scripts/CardInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CardInfo : MonoBehaviour
-{
-    public bool isEmpiled = false;
-    public int cardId;
-}
