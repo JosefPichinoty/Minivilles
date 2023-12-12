4794308245851a35526753bab654e2ee22948c1c
Bramzzrr <bramzz.frtn@gmail.com>
2023-12-12 14:47:40 +0100

    dq

--- Assets/SceneManger.cs
+++ Assets/SceneManger.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class SceneManger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string restartScene;
+    public string restart;
 }
