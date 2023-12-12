8aef04fc4cc631b5358f684c3d4599b9bcd05b8f
Bramzzrr <bramzz.frtn@gmail.com>
2023-12-12 15:20:44 +0100

    EndSceneWork

--- Assets/SceneManger.cs
+++ Assets/SceneManger.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManger : MonoBehaviour
 {
     public string restartScene;
-    public string restart;
+    public string menuScene;
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(restartScene);
+    }
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene(menuScene);
+    }
+
 }
